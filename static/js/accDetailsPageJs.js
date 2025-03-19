aa4243ca8d5ea1397f3ee45260a9a95f1c5c01d8
Anwar Hosen <anwarhossene@gmail.com>
2025-03-19 07:25:42 +0600

    added new payment modal

--- static/js/accDetailsPageJs.js
+++ static/js/accDetailsPageJs.js
@@ -15,3 +15,30 @@ window.addEventListener('load', () => {
         toggleSection(sectionId); // Open all sections
     });
 });
+
+
+
+
+// =========== Handles payment modal =====================
+const modal = document.getElementById('paymentModal');
+const openBtn = document.getElementById('openPaymentModal');
+const closeBtns = document.querySelectorAll('#closeModal, #cancelModal');
+
+// Modal Animation Handling
+function toggleModal(show) {
+    if(show) {
+        modal.classList.remove('hidden');
+        modal.classList.add('flex');
+        modal.children[0].style.animation = 'paymentModalIn 0.4s ease-in';
+    } else {
+        modal.children[0].style.animation = 'paymentModalOut 0.3s ease-out';
+        setTimeout(() => modal.classList.add('hidden'), 300);
+        setTimeout(() => modal.classList.remove('flex'), 300);
+    }
+}
+
+// Event Listeners
+openBtn.addEventListener('click', () => toggleModal(true));
+closeBtns.forEach(btn => btn.addEventListener('click', () => toggleModal(false)));
+modal.addEventListener('click', (e) => e.target === modal && toggleModal(false));
+document.addEventListener('keydown', (e) => e.key === 'Escape' && toggleModal(false));
